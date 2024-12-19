e20841ac94091af493b7b5c47fc3b33fdac43a40
Charly <charlyzz68@gmail.com>
2024-12-18 21:36:31 -0300

    calendar dias

--- resources/views/home/calendar.blade.php
+++ resources/views/home/calendar.blade.php
@@ -1,6 +1,28 @@
 <style>
+
+    .mes{
+      border-radius: 10px;
+      overflow: hidden;
+    }
     .mes .bar{
       font-size: 25px;
+    }
+
+    .semana{
+      border-radius: 10px;
+      border:solid thin white;
+    }
+
+    .semana .bar{
+      font-size: 20px;
+    }
+
+    .dia{
+      border-radius: 10px;
+    }
+
+    .state{
+      font-size: 30px;
     }
 </style>
 <div class="col-12 box-content p-1">
@@ -32,12 +54,12 @@
 
    log('meses', [meses])
 
-   function getLiDia(d){
-     var li = $('<li class="dia col-12 flex-row-between-start p-1">\
-                  <div class="col-10 flex-row-start-start flex-wrap p-1">\
-                      <div class="col-12 copa">copa</div>\
+   function getLiDia(d, col){
+     var li = $('<li class="dia col-12 flex-row-between-start p-1 mb-1">\
+                  <div class="box-fecha col-10 flex-row-start-start flex-wrap p-1">\
+                      <div class="col-12 copa-name">copa</div>\
                       <div class="col-3 flex-row-start-center">\
-                        <img src="{{ asset('resources/default/libertadores.png') }}" height="70">\
+                        <img class="copa" src="{{ asset('resources/default/libertadores.png') }}" height="70">\
                       </div>\
                       <div class="box-fecha col-9 flex-col-start-start">\
                         <b class="fase">fase</b>\
@@ -48,27 +70,65 @@
                     <b>sorteo</b>\
                     <div class="copas col-12 flex-row-around-center"></div>\
                   </div>\
-                <div class="box-inicio col-10 flex-row-start-center p-1">\
-                <div class="col-2 flex-col-center-center p-1">\
-                  <i class="fa fa-check"></i>\
-                  <i class="fa fa-check"></i>\
+                <div class="box-inicio col-10 flex-row-start-center p-1"></div>\
+                <div class="col-2 flex-col-center-center p-1 h-100">\
+                  <i class="state iniciada fa fa-check-circle"></i>\
+                  <i class="state procesada fa fa-times-circle mt-3"></i>\
                 </div>\
-              </li>')
+              </li>'),
+          copas = d.copas != null ? JSON.parse(d.copas) : []
+     li.find('.box-fecha, .box-sorteo, .box-inicio').hide()
+     li.find('.iniciada').hide()
+     switch(d.action){
+       case 'FECHA':
+        var copa = copas[0]
+        li.find('.copa-name').html(copa)
+        setImageCopa(li.find('.copa'), copa == 'afa' ? 'escudo_afa' : copa)
+        li.find('.box-fecha').show()
+        li.find('.fase').html(getNameFase(copa, d.fase))
+        li.find('.fecha').html(getNameFecha(d.fase, d.fecha))
+        li.find('.iniciada').show()
+        setCristalBorder(li, parseColor(col.a), parseColor(col.b), 1)
+       break
+       case 'SORTEO':
+        $.each(copas, function(i, c){
+          let img = $('<img height="70">')
+          setImageCopa(img, c == 'afa' ? 'escudo_afa' : c)
+          li.find('.copas').append(img)
+        })
+        li.find('.box-sorteo').show()
+        setCristalBorder(li, parseColor(col.b), parseColor(col.a), 1)
+       break
+      default:
+        li.find('.box-inicio').show()
+        setCristalBorder(li, parseColor(col.a), parseColor(col.b), 1)
+       break
+     }
+
+     
 
      return li
    }
 
-   function getLiSemana(sem){
-     var li = $('<li class="semana col-12 flex-col-start-center p-1">\
+   function getLiSemana(sem, col){
+     var li = $('<li class="semana col-12 flex-col-start-center p-1 mb-1">\
                   <div class="bar flex-row-center-center p-1"></div>\
                   <ul class="dias col-12 flex-col-start-center p-1 m-0"></ul>\
                 </li>')
+      li.find('.bar').html(sem.num + 'ª semana')
+
+      $.each(sem.dias, function(i, d){
+        li.find('.dias').append(getLiDia(d, col))
+      })
+
+      
+      setCristalBorder(li, parseColor(col.a), parseColor(col.b), 1)
 
       return li
    }
 
    function getLiMes(mes){
-      var li = $('<li class="mes col-12 flex-col-start-center cristal">\
+      var li = $('<li class="mes col-12 flex-col-start-center cristal mb-1">\
                   <div class="bar col-12 flex-row-center-center p-1"></div>\
                   <ul class="semanas col-12 flex-col-start-center p-1 m-0"></ul>\
                 </li>'),
@@ -76,6 +136,11 @@
       
       li.find('.bar').html(mes.name != null ? mes.name : 'inicio')
       setGradientUI(li.find('.bar'), col.a, col.b, .1)
+      setCristalRGB(li, parseColor(col.a))
+
+      $.each(mes.semanas, function(i, s){
+        li.find('.semanas').append(getLiSemana(s, col))
+      })
 
       return li
    }
